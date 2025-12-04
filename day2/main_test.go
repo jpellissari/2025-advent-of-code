2cf08bae7963d786bac7d56194434c8c8e1125f4
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2025-12-03 21:12:15 -0300

    feat: add part 1 of day2

---
+++ day2/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import "testing"
+
+func TestInvalidId(t *testing.T) {
+	testeCases := []struct {
+		name        string
+		r           IdRange
+		expectedSum int
+	}{
+		{
+			name: "Invalid IDs between 11-22 are 11 and 22, sum 33",
+			r: IdRange{
+				Start: 11, End: 22,
+			},
+			expectedSum: 33,
+		},
+	}
+
+	for _, tc := range testeCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := sumInvalidIdInRange(tc.r)
+			if result != tc.expectedSum {
+				t.Errorf("expected %v, got %v", tc.expectedSum, result)
+			}
+		})
+	}
+}
+
+func TestCheckRanges(t *testing.T) {
+	testeCases := []struct {
+		name        string
+		r           []IdRange
+		expectedSum int
+	}{
+		{
+			name: "Check AoC example range",
+			r: []IdRange{
+				{Start: 11, End: 22},
+				{Start: 95, End: 115},
+				{Start: 998, End: 1012},
+				{Start: 1188511880, End: 1188511890},
+				{Start: 222220, End: 222224},
+				{Start: 1698522, End: 1698528},
+				{Start: 446443, End: 446449},
+				{Start: 38593856, End: 38593862},
+			},
+			expectedSum: 1227775554,
+		},
+	}
+
+	for _, tc := range testeCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := checkRanges(tc.r)
+			if result != tc.expectedSum {
+				t.Errorf("expected %v, got %v", tc.expectedSum, result)
+			}
+		})
+	}
+}
