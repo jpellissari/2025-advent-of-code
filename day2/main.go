977fd1c2005dce5a5fc29d2c5d7d1fe8710ee2ba
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2025-12-03 21:12:15 -0300

    feat: add part 1 of day2

---
+++ day2/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type IdRange struct {
+	Start int
+	End   int
+}
+
+func main() {
+	ranges, err := parseInput()
+	if err != nil {
+		fmt.Printf("failed to parse input: %v", err)
+		os.Exit(1)
+	}
+
+	sum := checkRanges(ranges)
+
+	fmt.Println("Sum of invalid IDs: ", sum)
+}
+
+func checkRanges(ranges []IdRange) int {
+	totalSum := 0
+
+	for _, r := range ranges {
+		totalSum += sumInvalidIdInRange(r)
+	}
+
+	return totalSum
+}
+
+func sumInvalidIdInRange(r IdRange) int {
+	var sum int
+
+	for i := r.Start; i <= r.End; i++ {
+		s := strconv.Itoa(i)
+		isEven := len(s)%2 == 0
+		if isEven {
+			p1 := s[:len(s)/2]
+			p2 := s[len(s)/2:]
+
+			if p1 == p2 {
+				sum += i
+			}
+
+		}
+	}
+
+	return sum
+}
+
+func parseInput() ([]IdRange, error) {
+	content, err := os.ReadFile("./day2/input")
+	if err != nil {
+		return nil, err
+	}
+
+	var ranges []IdRange
+
+	content = []byte(strings.TrimSpace(string(content)))
+
+	rawRanges := strings.Split(string(content), ",")
+	for _, r := range rawRanges {
+		bounds := strings.Split(r, "-")
+
+		start, err := strconv.Atoi(bounds[0])
+		if err != nil {
+			fmt.Printf("failed to convert start id to int: %v", err)
+		}
+		end, err := strconv.Atoi(bounds[1])
+		if err != nil {
+			fmt.Printf("failed to convert end id to int: %v", err)
+		}
+
+		ranges = append(ranges, IdRange{Start: start, End: end})
+	}
+
+	return ranges, nil
+}
