ea79fe6c7fe25fa84a9bd9f2a77cf650bd75c9c8
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2025-12-05 08:23:55 -0300

    feat: solve part 2 of day2

--- day2/main_test.go
+++ day2/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestInvalidId(t *testing.T) {
+func TestInvalidIdPart1(t *testing.T) {
 	testeCases := []struct {
 		name        string
 		r           IdRange
@@ -19,7 +19,7 @@ func TestInvalidId(t *testing.T) {
 
 	for _, tc := range testeCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result := sumInvalidIdInRange(tc.r)
+			result := sumInvalidIdInRangePart1(tc.r)
 			if result != tc.expectedSum {
 				t.Errorf("expected %v, got %v", tc.expectedSum, result)
 			}
@@ -51,7 +51,50 @@ func TestCheckRanges(t *testing.T) {
 
 	for _, tc := range testeCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result := checkRanges(tc.r)
+			result := executePart1(tc.r)
+			if result != tc.expectedSum {
+				t.Errorf("expected %v, got %v", tc.expectedSum, result)
+			}
+		})
+	}
+}
+
+func TestSumInvalidIdPart2(t *testing.T) {
+	testeCases := []struct {
+		name        string
+		r           []IdRange
+		expectedSum int
+	}{
+		{
+			name: "Ignore cases where ID are all the same digit, but have multiple substrings",
+			r: []IdRange{
+				{Start: 2222, End: 2222},
+			},
+			expectedSum: 2222,
+		},
+
+		{
+			name: "Check AoC example range for part2",
+			r: []IdRange{
+				{Start: 11, End: 22},
+				{Start: 95, End: 115},
+				{Start: 998, End: 1012},
+				{Start: 1188511880, End: 1188511890},
+				{Start: 222220, End: 222224},
+				{Start: 1698522, End: 1698528},
+				{Start: 446443, End: 446449},
+				{Start: 38593856, End: 38593862},
+				{Start: 565653, End: 565659},
+				{Start: 824824821, End: 824824827},
+				{Start: 2121212118, End: 2121212124},
+			},
+			expectedSum: 4174379265,
+		},
+	}
+
+	for _, tc := range testeCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := executePart2(tc.r)
 			if result != tc.expectedSum {
 				t.Errorf("expected %v, got %v", tc.expectedSum, result)
 			}
